3693a996052f4cafe293637274636f33dc287343
Kolawole <onaelmangabo@gmail.com>
2021-06-16 20:38:54 +0200

    - add login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const classes = {
+    pageBody: "h-screen flex bg-gray-bg1",
+    formContainer:
+      "w-full max-w-md    m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16",
+    formHeading: "text-2xl  font-medium text-primary mt-4 mb-12 text-center",
+    btnContainer: "flex justify-center items-center mt-6",
+  };
+
+  return (
+    <div className={classes.pageBody}>
+      <div className={classes.formContainer}>
+        <h1 className={classes.formHeading}>Log in to your account 🔐</h1>
+
+        <form onSubmit={() => {}}>
+          <div className="space-y-4">
+            <input
+              className="border-b border-gray-300 w-full px-2 h-8 rounded focus:border-blue-500"
+              id="email"
+              type="email"
+              placeholder="Email"
+              name="email"
+            />
+            <input
+              className="border-b border-gray-300 w-full px-2 h-8 rounded focus:border-blue-500"
+              id="password"
+              type="password"
+              placeholder="Password"
+              name="password"
+            />
+          </div>
+
+          <div className={classes.btnContainer}>
+            <button
+              type="submit"
+              className="rounded border-gray-300 p-2 w-32 bg-blue-700 text-white"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
