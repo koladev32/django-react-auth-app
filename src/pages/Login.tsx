130d0094e7a02d2ff2ceb120fff96d2889f93f6a
Kolawole <onaelmangabo@gmail.com>
2021-06-16 21:23:05 +0200

    - feat: add login html

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 
 function Login() {
+
+  const handleLogin = (email: string, password: string) => {
+    console.log(email, password);
+  }
+
+  const handleFormSubmit = (e:any) => {
+    e.preventDefault();
+
+    let email = e.target.email?.value;
+    let password = e.target.element.password?.value;
+
+  }
+
   const classes = {
     pageBody: "h-screen flex bg-gray-bg1",
     formContainer:
-      "w-full max-w-md    m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16",
+      "w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16",
     formHeading: "text-2xl  font-medium text-primary mt-4 mb-12 text-center",
     btnContainer: "flex justify-center items-center mt-6",
   };
@@ -14,7 +27,7 @@ function Login() {
       <div className={classes.formContainer}>
         <h1 className={classes.formHeading}>Log in to your account 🔐</h1>
 
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleFormSubmit}>
           <div className="space-y-4">
             <input
               className="border-b border-gray-300 w-full px-2 h-8 rounded focus:border-blue-500"
