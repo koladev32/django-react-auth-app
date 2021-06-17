96a0c44ce43321e9fee10191bf02bd21d9e81936
Kolawole <onaelmangabo@gmail.com>
2021-06-18 00:16:59 +0200

    - feat: add redux-persist : create store

---
+++ src/store/index.ts
@@ -0,0 +1,38 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({});
+
+const persistedReducer = persistReducer(
+  {
+    key: "root",
+    version: 1,
+    storage: storage,
+  },
+  rootReducer
+);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default store;
